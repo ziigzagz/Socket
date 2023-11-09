1906c0e732912dabbe07a9446378dea05115e1cc
ziigzagz <iffan.hym@gmail.com>
2023-11-09 20:46:59 +0700

    update web socket

--- index.js
+++ index.js
@@ -8,23 +8,43 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
+function check_referrer(referrer) {
+  // true if referrer is not localhost
+  // false if referrer is localhost
+  if (referrer != 'http://localhost/') {
+    return true
+  }
+  return false
+}
 
-  socket.on('disconnect', () => {
-    console.log(`user is disconnected`)
+io.on('connection', (socket) => {
+  console.log('a user connected',socket.handshake.time)
+
+  socket.on('disconnect', (res) => {
+    console.log(`user is disconnected`, res)
   })
 
   socket.on('Create room ID', (res, callback) => {
     console.log('Created: ' + res.name)
+    if (check_referrer(res.referrer)) {
+      callback({
+        status: 404,
+      })
+    }
     callback({
       status: 200,
     })
 
     socket.on(res.room, (data, callback) => {
-      // socket.to(anotherSocketId).emit("private message", socket.id, msg);
+      if (check_referrer(data.referrer)) {
+        callback({
+          status: 404,
+        })
+      }
       console.log(`room : ${res.room} `, data)
-      io.emit(res.room, data)
+      if (data.msg.length <= 100) {
+        io.emit(res.room, data)
+      }
     })
   })
   // socket.on("private message", (anotherSocketId, msg) => {
